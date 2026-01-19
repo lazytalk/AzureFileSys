379ea85e5f36aae1e2710e10c9978b083e01d7a5
lazytalk <layztalk@gmail.com>
2026-01-19 11:50:02 +0800

    local env changed

--- tests/FileService.Tests/obj/Debug/net8.0/FileService.Tests.AssemblyInfo.cs
+++ tests/FileService.Tests/obj/Debug/net8.0/FileService.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FileService.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba705052134eff69da489fcb65b6a535b7ed6789")]
 [assembly: System.Reflection.AssemblyProductAttribute("FileService.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FileService.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
